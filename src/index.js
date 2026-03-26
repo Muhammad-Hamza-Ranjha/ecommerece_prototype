89a0655b72fb6319c3cd6dead32d6b870344c4f6
Muhammad-Hamza-Ranjha <ranjhahv04@gmail.com>
2026-03-26 21:02:59 +0500

    Update deployment settings and build assets

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="ecommerece_prototype">
+  <BrowserRouter basename="/ecommerece_prototype">
     <React.StrictMode>
       <App />
     </React.StrictMode>
